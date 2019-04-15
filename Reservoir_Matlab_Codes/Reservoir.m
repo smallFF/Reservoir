888aee894a961befb324a0231a092a60d0476fd0
smallFF <545030769@qq.com>
2019-04-15 11:44:56 +0800

    Matlab codes for reservior computing

--- Reservoir_Matlab_Codes/Reservoir.m
+++ Reservoir_Matlab_Codes/Reservoir.m
@@ -1,27 +1,32 @@
 classdef Reservoir < handle
     properties
-        M; %input nodes
-        N; %reservoir nodes
-        P; %output nodes
-        dataset_in;
-        dataset_out;
-        y_hat;
+        M;                   % input nodes
+        N;                   % reservoir nodes
+        P;                   % output nodes
+        t;                   % time
+        dataset_in;          % input data set
+        dataset_out;         % output data set
+        total_len;           % total data length
+        init_len;            % record states after init_len
+        train_len;           % training data length
+        
         degree_func = @(x) sqrt(x);
-        seed = 42;
+        seed = 42;           % random seed
         sigma = 1;
         bisa = 1;
         alpha = 1;
         beta = 1e-07;
-        total_len;
-        train_len;
-        t;
-        u;
-        s;
-        r;
-        Win;
-        Wout;
-        W;
-        S;
+        
+        u;                  % training input data set   
+        s;                  % training output data set
+        S;                  % predict dataset_out 
+        r;                  % reservoir states
+        R;                  % composition of u and r
+        Win;                % input weights
+        Wout;               % output weights
+        W;                  % reservoir weights
+
+        RMS;                % Root Mean Square errors
     end
     
     methods
@@ -35,18 +40,24 @@ classdef Reservoir < handle
             self.dataset_out = output;
             
             self.total_len = size(input, 2);
-            self.train_len = floor(0.7 * self.total_len);
+            self.init_len = 0;
+            self.train_len = floor(0.5 * self.total_len);
             rng(self.seed);
+            
             self.u = input(:, 1: self.train_len);
-            self.s = output(:, 1: self.train_len);
-            self.r = zeros(N, self.train_len);
+            self.s = output(:, self.init_len + 1: self.train_len);
+            self.S = zeros(self.P, self.total_len);
+            self.r = zeros(N, self.train_len - self.init_len);
+            self.R = zeros(1 + self.M + self.N, self.train_len - self.init_len);
             self.Win = -self.sigma + 2*self.sigma*rand(self.N, 1 + self.M);
-            self.S = zeros(1 + self.M + self.N, self.train_len);
+            self.Wout = [];
+            
             W = rand(N,N) - 0.5;
             opt.disp = 0;
             rhoW = abs(eigs(W, 1, 'LM', opt));
-            self.W = W .* (1.25/rhoW);
-            self.Wout = [];
+            self.W = W .* (1.0/rhoW);
+            
+            self.RMS = zeros(self.P, 1);
         end
             
         function train(self)
@@ -57,36 +68,42 @@ classdef Reservoir < handle
                uu = self.dataset_in(:, i + 1);
                self.r(:, i + 1) = (1 - self.alpha) * self.r(:, i) + ...
                 self.alpha * tanh( self.W * self.r(:,i) + self.Win *[1;uu]);
-               rr = self.r(:, i + 1); 
-               self.S(:, i + 1) = [1; uu; rr];
+               rr = self.r(:, i + 1);
+               
+               if i >= self.init_len
+                self.R(:, i + 1 - self.init_len) = [1; uu; rr];
+               end
+               
             end
-            disp('S : size()')
-            disp(size(self.S))
-            disp('S : size()')
-            disp(size(self.S'))
-            self.Wout = self.s * self.S' ...
-                / (self.S * self.S' + self.beta * eye(1 + self.M + self.N));
+
+            self.Wout = self.s * self.R' ...
+                / (self.R * self.R' + self.beta * eye(1 + self.M + self.N));
         end
         
         function predict(self)
-            S = zeros(1 + self.M + self.N, self.total_len);
+            RR = zeros(1 + self.M + self.N, self.total_len);
             rr = zeros(self.N, 1);
             for i = 1:self.total_len - 1
                uu = self.dataset_in(:, i + 1);
                rr = (1 - self.alpha) * rr + ...
                 self.alpha * tanh( self.W * rr + self.Win *[1;uu]);
-               S(:, i + 1) = [1; uu; rr];
+               RR(:, i + 1) = [1; uu; rr];
             end
             
-            self.y_hat = self.Wout * S;
+            self.S = self.Wout * RR;
+            
+            self.RMS = sqrt(sum((self.dataset_out - self.S).^2 ,2) ./ self.total_len);
+            
         end
         
         function draw(self)
+           figure();
            for i = 1:self.P
-              figure(i)
+              subplot(self.P, 1, i);
               plot(self.t, self.dataset_out(i,:), 'b-', ...
-                  self.t, self.y_hat(i,:),'r--')
+                  self.t, self.S(i,:),'r--')
               legend('true solution', 'predict solution');
+              xlabel(['RMS error = ',num2str(self.RMS(i))]);
            end
         end
     end
--- Reservoir_Matlab_Codes/my_example.m
+++ Reservoir_Matlab_Codes/my_example.m
@@ -1,7 +1,8 @@
+close all;
 lorenz = @(t, X) [-10*(X(1) - X(2));
                   28*X(1) - X(2) - X(1)*X(3);
                   X(1)*X(2) - 8/3 * X(3)];
-[time, y] = ode45(lorenz, [0, 20], [1, 1, 1]);
+[time, y] = ode45(lorenz, 0:0.01:50, [1, 1, 1]);
 % plot(time,y(:,1));
 y = (y-mean(y,1)) ./ mean((y - mean(y,1)).^2,1).^(1/2);
 input = y(:,1)';
